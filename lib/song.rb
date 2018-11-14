ee06954a0b05fec89c2ec69eaf4a7e9a4c258aef
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-11-14 13:34:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,9 +3,6 @@ class Song
   @@count = 0
   @@artists = []
   @@genres = []
-  @@genre_count = {}
-  
-  @@artist_count = {}
   
   def initialize(name, artist, genre)
     @@count += 1 
@@ -29,6 +26,7 @@ class Song
   end 
   
   def self.genre_count
+    hash = {}
     @genres.each do |genre|
       
     end 
