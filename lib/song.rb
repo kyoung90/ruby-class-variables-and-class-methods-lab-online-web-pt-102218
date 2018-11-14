533a024d51e8263c7a6b9694e2cb7945b6c09412
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-11-14 13:37:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
   def self.genre_count
     hash = {}
     @@genres.each do |genre|
-      if hash.has_key?
+      if hash.has_key?(genre)
         hash[genre] = 1
       else 
         hash[genre] += 1
